f95a3e821a4cf877dcd95f766049ac27f59886ae
Sinh Nguyen <sinh@sinhn.com>
2024-06-01 22:13:33 +0700

    Finish setup as guided video
    Fully setup neovim environment

--- lua/sinh/lazy.lua
+++ lua/sinh/lazy.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("sinh.plugins", {
+require("lazy").setup({ { import = "sinh.plugins" }, { import = "sinh.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
