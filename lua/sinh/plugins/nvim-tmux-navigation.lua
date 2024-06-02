c35f1bc8bb13ddacca6544a51018e596629ae188
Sinh Nguyen <sinh@sinhn.com>
2024-06-02 22:47:38 +0700

    Adjust navigation to use arrows instead of hjkl for easier use
    - This is due to the Colemak-dh layout have hjkl in different position
    compare to qwerty
    - Also as I use Sofle keyboard with arrow keys was maped to homerow-Nav
    layer which allow access to arrow keys with out hand movement

--- lua/sinh/plugins/nvim-tmux-navigation.lua
+++ lua/sinh/plugins/nvim-tmux-navigation.lua
@@ -10,10 +10,10 @@ return {
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
-    keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
-    keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
-    keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+    keymap.set('n', '<C-Left>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+    keymap.set('n', '<C-Down>', nvim_tmux_nav.NvimTmuxNavigateDown)
+    keymap.set('n', '<C-Up>', nvim_tmux_nav.NvimTmuxNavigateUp)
+    keymap.set('n', '<C-Right>', nvim_tmux_nav.NvimTmuxNavigateRight)
     keymap.set('n', '<C-\\>', nvim_tmux_nav.NvimTmuxNavigateLastActive)
     keymap.set('n', '<C-Space>', nvim_tmux_nav.NvimTmuxNavigateNext)
   end,
