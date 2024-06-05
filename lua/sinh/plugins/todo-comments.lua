bf0f3d56b48de7ce15a97840ebe40bbed973cff4
Sinh Nguyen <sinh@sinhn.com>
2024-06-05 10:08:38 +0700

    Initial setup Astro

--- lua/sinh/plugins/todo-comments.lua
+++ lua/sinh/plugins/todo-comments.lua
@@ -1,21 +1,16 @@
 return {
-  "folke/todo-comments.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
+  'folke/todo-comments.nvim',
+  event = { 'bufreadpre', 'bufnewfile' },
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local todo_comments = require("todo-comments")
-    
+    local todo_comments = require 'todo-comments'
+
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
+    keymap.set('n', ']t', function() todo_comments.jump_next() end, { desc = 'next todo comment' })
 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
-
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+    keymap.set('n', '[t', function() todo_comments.jump_prev() end, { desc = 'previous todo comment' })
 
     todo_comments.setup()
   end,
