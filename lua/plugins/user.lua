49dadc68177c01caa1b5aa1bf652d05f96a94ddd
Sinh Nguyen <sinh@sinhn.com>
2024-06-08 09:42:33 +0700

    Added hedyhly/outline.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -180,4 +180,16 @@ return {
     'nvim-neotree/neo-tree.nvim',
     enabled = false,
   },
+  -- code outline --
+  {
+    'hedyhli/outline.nvim',
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+      require('outline').setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 }
