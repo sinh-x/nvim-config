175bf26d24e91c1541d585f4d966c95fb339dd8e
Sinh Nguyen <sinh@sinhn.com>
2024-06-13 16:48:07 +0700

    Remove Leap

---
+++ lua/plugins/obsidian-bridge.lua
@@ -0,0 +1,17 @@
+-- stylua: ignore
+if true then return {} end
+
+return {
+  "oflisback/obsidian-bridge.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  config = function()
+    require("obsidian-bridge").setup({
+      obsidian_server_address = "http://localhost:27123",
+    })
+  end,
+  event = {
+    "BufReadPre *.md",
+    "BufNewFile *.md",
+  },
+  lazy = true,
+}
