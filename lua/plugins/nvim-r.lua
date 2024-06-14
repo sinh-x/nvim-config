dec78f2cbcd2354ee352e9e57b263fbf6f7dab0c
Sinh Nguyen <sinh@sinhn.com>
2024-06-15 06:26:21 +0700

    Update config on laptop

--- lua/plugins/nvim-r.lua
+++ lua/plugins/nvim-r.lua
@@ -16,8 +16,35 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
-      require("cmp").setup({ sources = { { name = "cmp_r" } } })
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "cmp_r" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "nvim_lsp" },
+          { name = "nvim_lua" },
+        },
+        completion = {
+          autocomplete = {
+            3, -- Autocomplete if the typed character is an identifier character
+          },
+        },
+      })
       require("cmp_r").setup({})
+    end,
+  },
+  {
+    "dense-analysis/ale",
+    config = function()
+      vim.g.ale_linters = { r = { "lintr" } }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
     end,
   },
 }
