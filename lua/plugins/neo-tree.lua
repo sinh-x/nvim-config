5974bd074fc352655c34712216f23b4e9d3b346f
Sinh Nguyen <sinh@sinhn.com>
2024-06-18 18:38:29 +0700

    Remove unnecessary comments

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,33 +1,26 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
- opts = {
+  opts = {
     window = {
       mappings = {
-               ["A"] = { 
-              "add_directory",
-              -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-              config = {
-                show_path = "relative" -- "none", "relative", "absolute"
-              }
-            },
-       ["a"] = { 
-              "add",
-              -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-              config = {
-                show_path = "relative" -- "none", "relative", "absolute"
-              }
-            },
-        ["m"] = { 
-              "move",
-              -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-              config = {
-                show_path = "relative" -- "none", "relative", "absolute"
-              }
-            },
-
+        ["A"] = {
+          "add_directory",
+          config = {
+            show_path = "relative",             -- "none", "relative", "absolute"
+          },
+        },
+        ["a"] = {
+          "add",
+          config = {
+            show_path = "relative",             -- "none", "relative", "absolute"
+          },
+        },
+        ["m"] = {
+          "move",
+          config = {
+            show_path = "relative",             -- "none", "relative", "absolute"
+          },
+        },
       },
     },
   },
